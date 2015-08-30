a5fa42248af41a72927fd95b2aada9ca140edda5
Adam Nelson <anelson@apocryph.org>
2015-08-29 23:34:53 -0400

    Add more generation options Capture some thoughts in the README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # scalydomain
 Implementation of my toy domain name generator idea but in Scala
+
+This is a collection of experiments into the viability of generating domain names using various novel algorithms.
+
+It assumes the .com zonefile is available.
+
+Note that some domains, for example 'velk.com', are registered but do not appear in the zonefile.  This seems rare, but it must be considered a possibility.  A more comprehensive solution would need to submit WHOIS queries for candidate domains that do not appear in the zonefile, in order to be sure they are truly available.
+
+I'm trying various ngram lengths.  Not surprisingly, 2-grams are the most random-looking.  Even at five chars, the output is too random to be worthwhile.
+
+I find 4-grams to be pretty useful.  They found me domains like null5.com and 2jack.com, among others.
+
+Training on domain names, and not words, means that even 4-grams have seemingly random chars.  For example, I just generated LLLLL domains with 4-grams, and got results like xvsyg, hpgjb, jdkei, etc.  This means there are domains that start with 'xvsy', 'jdke', etc.  Training on a dictionary word corpus would not produce these results.
+
+I'm discovering that it's not hard to generate lots of possibilities with various rules.  The challenge is sorting the results and filtering out the junk, like above.  I have a few ideas about this:
+
+* Score domains based on what proportion of the string contains dictionary words.  This is computationally non-trivial but it would be worthwhile
+* Train another 2-gram or 3-gram model on the dictionary, then measure the domain by the probabilities of that dictionary-trained model generating the domain.
--- generate/main.scala
+++ generate/main.scala
@@ -6,6 +6,7 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import scala.collection.JavaConversions._
 import scala.collection.mutable.SortedSet
+import scala.util.matching.Regex
 import ExecutionContext.Implicits.global
 
 import scalydomain.core.DomainDb
@@ -17,9 +18,25 @@ case class CliOptions(domainDbFile: File = new File("."),
 	ngramSize: Int = -1,
 	prefix: String = "",
 	maxLength: Int = -1,
-	domainsToGenerate: Int = 20)
+	domainsToGenerate: Int = 20,
+	pattern: Option[Regex] = None)
 
 object Generate {
+	def compilePattern(pattern: String) = {
+		val re = pattern.map { char =>
+			char match {
+				case 'L' => "[a-z]"
+				case 'N' => "\\d"
+				case 'V' => "[aeiouy]" //TODO: Is there a way to use Unicode char class to make this work for other writing systems?
+				case 'C' => "[^aeiou]"
+				case x => x
+			}
+		}.mkString
+
+		println(s"Limiting domains to those matching regex $re")
+		Some(new Regex("^" + re + "$"))
+	}
+
   def main(args: Array[String]): Unit = {
 		val optParser = new scopt.OptionParser[CliOptions]("generate") {
 		  head("generate", "SNAPSHOT")
@@ -35,6 +52,8 @@ object Generate {
 		    c.copy(maxLength = x) } text("Generate only domain names that are no longer than this")
 		  opt[Int]('c', "count") optional() action { (x, c) =>
 		    c.copy(domainsToGenerate = x) } text("Generate this many domains")
+		  opt[String]('f', "pattern") optional() action { (x, c) =>
+		    c.copy(pattern = compilePattern(x)) } text("Generate domains that match this pattern (LNCV plus regex)")
 		}
 
   	val config = optParser.parse(args, CliOptions()).get
@@ -52,7 +71,7 @@ object Generate {
 
 				do {
 					generated = markov.generate(config.maxLength, config.prefix)
-				} while (domainDb.domainExists(generated) || generatedNames.contains(generated))
+				} while (!acceptableDomain(config, domainDb, generatedNames, generated))
 
 				generatedNames += generated
 				println(s"Generated available domain $generated")
@@ -61,6 +80,17 @@ object Generate {
 			domainDb.close
 			modelDb.close
 		}
+  }
+
+  def acceptableDomain(config: CliOptions, domainDb: DomainDb, generatedNames: SortedSet[String], domain: String) = {
+  	(
+	  	!domainDb.domainExists(domain) &&
+	  	!generatedNames.contains(domain) &&
+	  	(config.pattern match {
+	  		case Some(re) => !re.findPrefixOf(domain).isEmpty
+	  		case None => true
+	  	})
+  	)
   }
 }
 
