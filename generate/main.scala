60dd59f0d7509686ddadb0d03849fe5710e5912b
Adam Nelson <anelson@apocryph.org>
2015-08-31 17:42:23 -0400

    Implement sorting of matches by their score Implement a novel scoring algorithm based on comparing lowest probability letters between words in a sequence.  still not happy with the result

--- generate/main.scala
+++ generate/main.scala
@@ -6,6 +6,7 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import scala.collection.JavaConversions._
 import scala.collection.mutable.SortedSet
+import scala.math.pow
 import scala.util.matching.Regex
 import ExecutionContext.Implicits.global
 
@@ -19,7 +20,8 @@ case class CliOptions(domainDbFile: File = new File("."),
 	maxLength: Int = -1,
 	domainsToGenerate: Int = 20,
 	pattern: Option[Regex] = None,
-	includeWords: String = "")
+	includeWords: String = "",
+	sort: Boolean = false)
 
 object Generate {
 	def compilePattern(pattern: String) = {
@@ -54,6 +56,8 @@ object Generate {
 		    c.copy(pattern = compilePattern(x)) } text("Generate domains that match this pattern (LNCV plus regex)")
 		  opt[String]('i', "include") optional() action { (x, c) =>
 		    c.copy(includeWords = x) } text("Include these words in the generated output")
+		  opt[Unit]('s', "sort") optional() action { (x, c) =>
+		    c.copy(sort = true) } text("Sort output with highest score first")
 		}
 
   	val config = optParser.parse(args, CliOptions()).get
@@ -68,17 +72,37 @@ object Generate {
 			println("Generating domain names")
 
 			val markovGeneratedDomains = Stream.continually { markov.generate(config.maxLength, config.prefix) }
-			val userSpecifiedDomains = config.includeWords.split(",")
+			val userSpecifiedDomains = config.includeWords.split(",").filter(_.length > 0)
 			val domainHose = userSpecifiedDomains.toStream #::: markovGeneratedDomains
 			val acceptableDomains = domainHose.filter(acceptableDomain(config, domainDb, generatedNames, _))
+			val domainsWithScores = acceptableDomains.map { domain =>
+				val p = markov.computeCharacterProbabilities(domain).toArray
+				//val score = p.foldLeft(1.0)(_*_)
+				//val score = p.map(pow(_, 2)).foldLeft(1.0)(_*_)
+				val scores = p.sorted
 
-			acceptableDomains.take(config.domainsToGenerate).foreach { domain =>
+				val charProbabilities = (domain+"$").zip(p).map { case (c, prob) => f"P($c)=$prob%4f" }.mkString(",")
+
+				(domain, scores, charProbabilities)
+			}
+
+			val ordering = new Ordering[(String, Array[Double], String)] {
+				def compare(x: (String, Array[Double], String), y: (String, Array[Double], String)) : Int = {
+					x._2.zip(y._2).dropWhile(tuple => tuple._1 == tuple._2).headOption match {
+						case Some((lhs, rhs)) => Ordering[Double].compare(rhs, lhs)
+						case None => Ordering[Int].compare(y._2.length, x._2.length)
+					}
+				}
+			}
+
+			val domainOutput = config.sort match {
+				case true => domainsWithScores.take(config.domainsToGenerate).toArray.sorted(ordering).toStream
+				case false => domainsWithScores.take(config.domainsToGenerate).toStream
+			}
+
+			domainOutput.foreach { case (domain, _, charProbabilities) =>
 				generatedNames += domain
-				val p = markov.computeNgramProbabilities(domain).toArray
-				val score = p.min
-				val ngramProbabilities = domain.sliding(ngramSize).toArray.zip(p).map { case (ngram, prob) => f"P($ngram)=$prob%6f" }.mkString(",")
-
-				println(f"\t$domain\t$score%6f\t$ngramProbabilities")
+				println(f"\t$domain\t$charProbabilities")
 			}
 		} finally {
 			domainDb.close
