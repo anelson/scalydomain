cd07b1ae6e817ab56f81f9534d7e88a7eab99963
Adam Nelson <anelson@apocryph.org>
2015-09-01 13:28:36 -0400

    Train wordlist-based Markov models with the superior Norvig wordlists

--- bin/train-dictionary.sh
+++ bin/train-dictionary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Trains 2-, 3-, and 4-gram markov models from the wordlist
-sbt "train/run --textfile data/wordlist --modeldb data/dict-markov-2 --ngram 2"
-sbt "train/run --textfile data/wordlist --modeldb data/dict-markov-3 --ngram 3"
-sbt "train/run --textfile data/wordlist --modeldb data/dict-markov-4 --ngram 4"
+sbt "train/run --textfile data/wordlist-top10000 --modeldb data/dict-markov-2 --ngram 2"
+sbt "train/run --textfile data/wordlist-top10000 --modeldb data/dict-markov-3 --ngram 3"
+sbt "train/run --textfile data/wordlist-top10000 --modeldb data/dict-markov-4 --ngram 4"
